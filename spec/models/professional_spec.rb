fbd7b20a4b26979ef37cd82fd74fe5c562ae821a
Ivan <ipfaab@fi.uba.ar>
2022-09-17 19:32:16 +0000

    Wrapping up the amount to pay for the professional class

--- spec/models/professional_spec.rb
+++ spec/models/professional_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Professional do
   describe 'amount_to_pay' do
-    it 'should be 0 when no active offers' do
+    it 'should be 30.0 when no active offers' do
       professional = described_class.new
       amount_to_pay = professional.amount_to_pay(0)
-      expect(amount_to_pay).to eq 0.0
+      expect(amount_to_pay).to eq 30.0
     end
 
     it 'should be 30.0 when active offers are 4' do
@@ -14,13 +14,13 @@ describe Professional do
       expect(amount_to_pay).to eq 30.0
     end
 
-    xit 'should be 30.0 when active offers are 5' do
+    it 'should be 30.0 when active offers are 5' do
       professional = described_class.new
       amount_to_pay = professional.amount_to_pay(5)
       expect(amount_to_pay).to eq 30.0
     end
 
-    xit 'should be 44.0 when active offers are 7' do
+    it 'should be 44.0 when active offers are 7' do
       professional = described_class.new
       amount_to_pay = professional.amount_to_pay(7)
       expect(amount_to_pay).to eq 44.0
